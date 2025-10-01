cd0dda06ecb8a0440a5b781e58d457215423f4a0
sanjay <sanjaykrishna.blubridge@evoplus.in>
2025-10-01 16:51:05 +0530

    updated th UnaryOps.h file

--- include/UnaryOps.h
+++ include/UnaryOps.h
@@ -48,8 +48,23 @@ void atanh_(Tensor& x);
 
 
 // exponentials and logarithms
+// ============================================================
+// Out-of-place unary trigonometric functions
+// ============================================================
+Tensor exp(const Tensor& input);
+Tensor exp2(const Tensor& input);
+Tensor log(const Tensor& input);
+Tensor log2(const Tensor& input);
+Tensor log10(const Tensor& input);
 
-
+// ============================================================
+// In-place unary trigonometric functions
+// ============================================================
+void exp_(Tensor& input);
+void exp2_(Tensor& input);
+void log_(Tensor& input);
+void log2_(Tensor& input);
+void log10_(Tensor& input);
 
 
 
